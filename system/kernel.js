86e9e2db879d10cb3828b761b0dee50a78fa7af5
TechWorldInc <76709090+TechWorldInc@users.noreply.github.com>
2021-08-06 23:27:27 +0200

    Update singleInstance

--- system/kernel.js
+++ system/kernel.js
@@ -12,68 +12,69 @@ let { app, BrowserWindow } = require('electron')
 
 if (!app.requestSingleInstanceLock()) {
     app.quit();
-}
+} else {
 
-app.on("ready", () => {
-    let { app, globalShortcut } = require('electron')
+    app.on("ready", () => {
+        let { app, globalShortcut } = require('electron')
 
-    globalShortcut.register("Alt+Enter", () => {
-        if (BrowserWindow.getFocusedWindow().fullScreen == true) {
-            BrowserWindow.getFocusedWindow().fullScreen = false;
-        } else {
-            BrowserWindow.getFocusedWindow().fullScreen = true;
+        globalShortcut.register("Alt+Enter", () => {
+            if (BrowserWindow.getFocusedWindow().fullScreen == true) {
+                BrowserWindow.getFocusedWindow().fullScreen = false;
+            } else {
+                BrowserWindow.getFocusedWindow().fullScreen = true;
+            }
+        })
+
+        globalShortcut.register('Control+Shift+I', () => {
+            BrowserWindow.getFocusedWindow().toggleDevTools();
+        })
+        globalShortcut.register("F4", () => {
+            return false;
+        })
+
+        globalShortcut.register("Control+Alt+Shift+R", () => {
+            win.loadFile("main.html")
+        })
+        win = new BrowserWindow({
+            width: 800,
+            height: 600,
+            frame: true,
+            minWidth: 1000,
+            minHeight: 700,
+            fullscreen: true,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false,
+                enableRemoteModule: true,
+                devTools: true
+            },
+            backgroundColor: "#000",
+        })
+
+        win.removeMenu();
+
+        loadStartPage();
+
+        win.on("maximize", () => {
+            win.unmaximize();
+            setTimeout(() => {
+                win.fullScreen = true;
+            }, 50);
+        })
+    });
+
+
+    function loadStartPage() {
+        switch (start) {
+            case 0:
+                win.loadFile("main.html");
+                break;
+            case 1:
+                win.loadFile("login.html");
+                break;
+            case 2:
+                win.loadFile("arcos.html");
+                break;
         }
-    })
-
-    globalShortcut.register('Control+Shift+I', () => {
-        BrowserWindow.getFocusedWindow().toggleDevTools();
-    })
-    globalShortcut.register("F4", () => {
-        return false;
-    })
-
-    globalShortcut.register("Control+Alt+Shift+R", () => {
-        win.loadFile("main.html")
-    })
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        frame: true,
-        minWidth: 1000,
-        minHeight: 700,
-        fullscreen: true,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: true,
-            devTools: true
-        },
-        backgroundColor: "#000",
-    })
-
-    win.removeMenu();
-
-    loadStartPage();
-
-    win.on("maximize", () => {
-        win.unmaximize();
-        setTimeout(() => {
-            win.fullScreen = true;
-        }, 50);
-    })
-});
-
-
-function loadStartPage() {
-    switch (start) {
-        case 0:
-            win.loadFile("main.html");
-            break;
-        case 1:
-            win.loadFile("login.html");
-            break;
-        case 2:
-            win.loadFile("arcos.html");
-            break;
     }
 }
